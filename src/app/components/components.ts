fc0e1ba1dc4ff658c7755c9c979178515b321417
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2025-06-21 09:32:59 +0200

    two way binding

--- src/app/components/components.ts
+++ src/app/components/components.ts
@@ -1,14 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { InternalComponent } from './internal-component/internal-component';
 import { FormsModule } from '@angular/forms';
+import { TwoWayBinding } from "./two-way-binding/two-way-binding";
 
 @Component({
   selector: 'app-components',
-  imports: [InternalComponent, FormsModule],
+  imports: [InternalComponent, FormsModule, TwoWayBinding],
   templateUrl: './components.html',
   styleUrl: './components.scss'
 })
 export class Components {
   testInput = signal("Test input from Components");
 
+  textChange = (value: string) => {
+    console.log("Text changed to: ", value);
+  }
 }
---
+++ src/app/components/two-way-binding/two-way-binding.ts
@@ -0,0 +1,12 @@
+import { Component, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-two-way-binding',
+  imports: [FormsModule],
+  templateUrl: './two-way-binding.html',
+  styleUrl: './two-way-binding.scss'
+})
+export class TwoWayBinding {
+  text = model('')
+}
