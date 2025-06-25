4d5f47a1952b1db71658785aae51bfa580afcff2
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2025-06-25 15:49:00 +0200

    focus element

--- src/app/http-test/form-template/form-template.ts
+++ src/app/http-test/form-template/form-template.ts
@@ -1,8 +1,8 @@
-import { Component, computed, inject, input, model, OnInit, signal } from '@angular/core';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { ChangeDetectorRef, Component, ElementRef, inject, input, OnInit, signal, viewChild } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { PostService } from '../../services/post-service';
 import Post from '../../models/Post';
-import { FormsModule, NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { TestValidationDirective } from '../../test-validation-directive';
 
 @Component({
@@ -15,12 +15,20 @@ export class FormTemplate implements OnInit {
 
   private postService = inject(PostService);
   private router = inject(Router)
+  private cdr = inject(ChangeDetectorRef);
 
   id = input.required<number>()
   post = signal<{ loading: true } | { loading: false, post: Post }>({ loading: true })
+  titleRef = viewChild<ElementRef<HTMLInputElement>>('titleRef');
 
   ngOnInit(): void {
-    this.postService.getPost(this.id()).subscribe(x => this.post.set({ loading: false, post: x }));
+    this.postService.getPost(this.id())
+      .subscribe(x => {
+        this.post.set({ loading: false, post: x });
+        this.cdr.detectChanges();
+        this.titleRef()?.nativeElement.focus();
+      });
+
   }
   onSubmit() {
     const post = this.post()
