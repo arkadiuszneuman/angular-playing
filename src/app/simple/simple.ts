b2932e944997053923242de91ec841ea9c871b04
Arkadiusz Neuman <arkadiusz.neuman@gmail.com>
2025-06-26 16:26:42 +0200

    containers

--- src/app/simple/simple.ts
+++ src/app/simple/simple.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, TemplateRef, viewChild, ViewContainerRef } from '@angular/core';
 import { TestStructuralDirective } from '../test-structural-directive';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-simple',
-  imports: [TestStructuralDirective],
+  imports: [TestStructuralDirective, NgTemplateOutlet],
   templateUrl: './simple.html',
-  styleUrl: './simple.scss'
+  styleUrl: './simple.scss',
+  host: {
+    'role': 'slider'
+  }
 })
-export class Simple {
+export class Simple implements OnInit {
+  testTemplate = viewChild("testTemplate", { read: TemplateRef })
+  testContainer = viewChild('testContainer', { read: ViewContainerRef })
+
+  ngOnInit(): void {
+    const template = this.testTemplate()
+    if (template) {
+      this.testContainer()?.createEmbeddedView(template)
+    }
+  }
 
 }
